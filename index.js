8838518a9f2f403e47a9388ff99ba67efa84fc6a
Jaemok <ashnamuh@gmail.com>
2019-07-11 14:32:34 +0900

    add rules

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    'object-shorthand': ['error', 'always'],
+    'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
+    'no-unused-vars': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'only-multiline'],
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'key-spacing': ['error', { 'beforeColon': false }]
+  },
+}
